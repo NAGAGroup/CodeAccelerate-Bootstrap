29b60beab80ff3624b1cdc8ee05f6f0f837fd81b
Jack Myers <jack.myers@intel.com>
2024-07-31 19:54:22 +0000

    better fish config and added community language packs to nvim

--- Mackup/.config/nvim/lua/community.lua
+++ Mackup/.config/nvim/lua/community.lua
@@ -6,8 +6,10 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.pack.cpp" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
+	{ import = "astrocommunity.pack.cpp" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.cmake" },
 }
