5bbf2a3487702a72cb83d7889a7fb5f3ea7c6d4e
Jack Myers <jackhmyers97@gmail.com>
2024-09-08 22:50:02 -0700

    switched to tabby+ollama!

--- home/private_dot_config/nvim/lua/plugins/codecompanion.lua
+++ home/private_dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,63 +1,3 @@
--- return {
---   "robitx/gp.nvim",
---   -- A plugin configuration to enhance your coding experience!
---   config = function()
---     -- Define custom configurations for each provider
---     local conf = {
---       providers = {
---         -- Use OpenAI models for AI-driven suggestions
---         openai = {},
---
---         -- Utilize Azure's cognitive services for intelligent completions
---         azure = {},
---
---         -- Activate CoPilot-powered code completion
---         copilot = {},
---
---         -- Disable or customize codellama-powered completions
---         codellama = {
---           disable = false,
---           endpoint = "http://localhost:11434/api/chat" -- Secret key for authentication
---         },
---
---         -- Integrate with LmStudio's language models
---         lmstudio = {},
---
---         -- Leverage Google AI services for enhanced code suggestions
---         googleai = {},
---
---         -- Access PPLX's proprietary programming knowledge base
---         pplx = {},
---
---         -- Tap into Anthropic's AI-driven coding assistance
---         anthropic = {}
---       },
---       agents = {
---         {
---           provider = "codellama",
---           name = "codellama",
---           chat = true,
---           command = false,
---           -- system prompt (use this to specify the persona/role of the AI)
---           system_prompt =
---           "You are a general code assistant",
---           -- string with model name or table with model name and parameters
---           model = {
---             model = "codellama:7b",
---           }
---         }
---       }
---     }
---
---     -- Load the GP plugin with custom configurations
---     require("gp").setup(conf)
---
---     -- Define shortcuts for an enhanced user experience (see Documentation/Readme)
---   end
--- }
-
-
-
 return {
   "codecompanion.nvim",
   dependencies = {
@@ -70,7 +10,7 @@ return {
   },
 
   config = function()
-    require("codecompanion").setup({
+    return require("codecompanion").setup({
       strategies = {
         chat = {
           adapter = "codellama",
@@ -87,16 +27,17 @@ return {
         },
       },
       adapters = {
-        codellama = require("codecompanion.adapters").extend("ollama", {
-          name = "codellama", -- Ensure the model is differentiated from Ollama
-          schema = {
-            model = {
-              default = "codellama:7b",
-            }
-          },
-        })
+        codellama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            name = "codellama", -- Ensure the model is differentiated from Ollama
+            schema = {
+              model = {
+                default = "codellama:7b",
+              }
+            },
+          })
+        end,
       },
     })
-    return true
   end
 }
--- home/private_dot_config/nvim/lua/plugins/tabby.lua
+++ home/private_dot_config/nvim/lua/plugins/tabby.lua
@@ -1,5 +1,8 @@
 return {
-  -- "TabbyML/vim-tabby",
-  -- event = "BufEnter",
-  -- init = function() vim.g.tabby_keybinding_accept = '<C-]>' end
+  "TabbyML/vim-tabby",
+  event = "BufEnter",
+  init = function()
+    vim.g.tabby_keybinding_reject = '<C-Backslash>'
+    vim.g.tabby_keybinding_accept = '<C-]>'
+  end
 }
