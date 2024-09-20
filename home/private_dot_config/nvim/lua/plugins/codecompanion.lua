2d51f08840f7e3102305017508b8ed3c8c3065a3
Jack Myers <jackhmyers97@gmail.com>
2024-09-19 21:13:10 -0700

    updates
    Signed-off-by: Jack Myers <jackhmyers97@gmail.com>

--- home/private_dot_config/nvim/lua/plugins/codecompanion.lua
+++ home/private_dot_config/nvim/lua/plugins/codecompanion.lua
@@ -32,7 +32,19 @@ return {
             name = "codellama", -- Ensure the model is differentiated from Ollama
             schema = {
               model = {
-                default = "codellama:7b",
+                default = "deepseek-coder:6.7b",
+              },
+              num_ctx = {
+                order = 3,
+                mapping = "parameters.options",
+                type = "number",
+                optional = false,
+                default = 8096,
+                desc =
+                "The maximum number of tokens that the language model can consider at once. This determines the size of the input context window, allowing the model to take into account longer text passages for generating responses. Adjusting this value can affect the model's performance and memory usage.",
+                validate = function(n)
+                  return n > 0, "Must be a positive number"
+                end,
               }
             },
           })
