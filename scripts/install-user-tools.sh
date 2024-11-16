a2459d59d5fbeaa4e2857e018aae599fc899efb3
Jack Myers <jack.myers@intel.com>
2024-11-16 00:03:11 +0000

    remove broken parts of install script on linux
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- scripts/install-user-tools.sh
+++ scripts/install-user-tools.sh
@@ -7,24 +7,4 @@ sh -c "$(curl -fsLS get.chezmoi.io)" -- -b ~/bin
 
 pixi global sync
 
-~/.pixi/bin/nu -c 'bash -c "
-
-npm install --global tabby-agent
-
-if [ ! -d ~/.local/share/omf ]; then
-	fish -c "$(curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install)" -- --noninteractive
-fi
-
-system_pkgmanager=""
-if command -v apt >/dev/null 2>&1; then
-	system_pkgmanager="apt"
-else
-	command -v dnf >/dev/null 2>&1
-	system_pkgmanager="dnf"
-fi
-
-if ! command -v kitty >/dev/null 2>&1; then
-	sudo $system_pkgmanager install -y kitty
-fi
-
-echo "source ~/.config/envvars.sh" | tee -a ~/.bashrc"'
+~/.pixi/bin/nu -c 'npm install --global tabby-agent'
