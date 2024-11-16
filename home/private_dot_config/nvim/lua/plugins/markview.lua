5834d4dc7d3ef453d8612659a0df6bee12059ce5
Jack Myers <jack.myers@intel.com>
2024-11-16 02:23:32 +0000

    switch to markview nvim
    Signed-off-by: Jack Myers <jack.myers@intel.com>

---
+++ home/private_dot_config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		enabled = false,
+	},
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false, -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+}
