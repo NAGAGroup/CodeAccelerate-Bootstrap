f8a3d7a1e9249a837822af49ed871a3540ccf058
Jack Myers <jack.myers@intel.com>
2024-12-11 00:30:28 +0000

    explicit enable/disable of snacks
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- home/private_dot_config/nvim/lua/plugins/snacks.lua
+++ home/private_dot_config/nvim/lua/plugins/snacks.lua
@@ -1,7 +1,16 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = true },
 		notifier = {
+			enabled = true,
 			style = "fancy",
 			top_down = false, -- place notifications from top to bottom
 		},
