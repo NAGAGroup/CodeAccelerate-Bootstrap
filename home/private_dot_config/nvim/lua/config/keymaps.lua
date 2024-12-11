344728280ecbc43b8b44002c0335cd7132e32964
Jack Myers <jack.myers@intel.com>
2024-12-11 00:30:21 +0000

    added codecompanion keymap for chat
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- home/private_dot_config/nvim/lua/config/keymaps.lua
+++ home/private_dot_config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,5 @@
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
 map("n", "<leader>snH", "<cmd>lua Snacks.notifier.show_history()<cr>", { desc = "Snacks Notification History" })
+map("n", "<leader>cAC", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "Chat with CodeCompanion" })
 -- map("n", "<leader>uT", "<cmd>lua require('nvchad.themes').open()<cr>", { desc = "NVChad Theme Picker" })
