d80d29cc89d1ebaebdfe08b56c78fca695f37f21
Jack Myers <jackhyers97@gmail.com>
2024-12-14 22:18:09 -0800

    updated bootstrap scripts
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,2 +1,4 @@
 pixi run install
 pixi global sync
+
+%USERPROFILE%\.pixi\envs\dev\bin\nu -n -c "%USERPROFILE%\.pixi\envs\dev\bin\cargo install --locked nu"
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,13 @@ set -exou -pipefail
 
 pixi run install
 pixi global sync
+
+mkdir -p carapace-bin
+wget https://github.com/carapace-sh/carapace-bin/releases/download/v1.1.0/carapace-bin_1.1.0_linux_amd64.tar.gz -O carapace-bin.tar.gz
+tar -xzvf carapace-bin.tar.gz -C carapace-bin
+cp carapace-bin/carapace ~/bin
+rm -rf carapace-bin.tar.gz carapace-bin
+
+find ~/bin -type f -exec chmod +x {} \;
+
+~/.pixi/bin/nu -n -c "$HOME/.pixi/envs/dev/bin/cargo install --locked nu; $HOME/.pixi/envs/dev/bin/cargo install --locked zellij"
