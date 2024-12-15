e6bef09107edbaa4e26dc12e3b94ba844326a75c
Jack Myers <jackhyers97@gmail.com>
2024-12-14 22:27:27 -0800

    added nushell support
    Signed-off-by: Jack Myers <jackhyers97@gmail.com>

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 		{ import = "lazyvim.plugins.extras.editor.mini-diff" },
+		{ import = "lazyvim.plugins.extras.lang.nushell" },
 		{ import = "plugins" },
 	},
 	defaults = {
