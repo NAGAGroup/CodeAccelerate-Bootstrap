86fae3b7ef5a533faa2ed92530275b3d2c1cbb36
Jack Myers <jack.myers@intel.com>
2024-12-21 15:00:36 -0800

    switched windows finalization script to nu

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,4 +1,4 @@
 pixi run install
 pixi global sync
 
-%USERPROFILE%/.pixi/bin/bash scripts/finalize-windows.sh
+nu scripts/finalize-windows.nu
--- scripts/finalize-windows.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-cargo="$HOME/.pixi/bin/cargo.bat"
-
-$cargo install --locked nu
