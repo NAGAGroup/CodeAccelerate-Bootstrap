286bf2f61e5d614692abde91f802707838a98370
Jack <jackhmyers97@gmail.com>
2025-02-04 02:51:55 -0800

    WIP
    Signed-off-by: Jack <jackhmyers97@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,7 +13,6 @@ rm -rf carapace-bin.tar.gz carapace-bin
 
 find ~/bin -type f -exec chmod +x {} \;
 
-cargo install --locked nu
-cargo install zellij
+cargo install --git https://github.com/prefix-dev/shell.git --tag v0.2.0 --locked shell
 
 ln -sf ~/.cargo/bin/nu ~/.pixi/bin/nu
--- scripts/finalize-windows.nu
+++ scripts/finalize-windows.nu
@@ -1,6 +1,6 @@
-cargo install --locked nu
-
 rm -rf ~/AppData/Local/nvim
 ln -sf ~/.config/nvim ~/AppData/Local/nvim
 
 powershell.exe ./scripts/install-fonts.ps1
+
+cargo install --git https://github.com/prefix-dev/shell.git --tag v0.2.0 --locked shell
