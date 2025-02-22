d2d532128444fc205418c2513f999767ec5b1003
Jack Myers <jack.myers@intel.com>
2025-02-22 00:23:12 +0000

    WIP
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- home/private_dot_config/nvim/lua/config/lazy.lua
+++ home/private_dot_config/nvim/lua/config/lazy.lua
@@ -45,7 +45,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.mini-files" },
 		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "lazyvim.plugins.extras.coding.mini-surround" },
-		{ import = "lazyvim.plugins.extras.editor.mini-diff" },
 		{ import = "lazyvim.plugins.extras.lang.nushell" },
 		{ import = "lazyvim.plugins.extras.util.octo" },
 		{ import = "plugins" },
