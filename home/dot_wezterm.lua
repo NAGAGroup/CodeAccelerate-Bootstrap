7aab637c083a924cbcd4338cfbe2ee4de0fb86eb
Jack Myers <jack.myers@intel.com>
2025-07-10 21:20:17 +0000

    Update WezTerm and LazyGit configurations

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -4,10 +4,9 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
-config.color_scheme = "Ayu Dark (Gogh)"
+config.color_scheme = "Horizon Dark (base16)"
+-- config.color_scheme = "rose-pine"
 
 config.front_end = "WebGpu"
 
@@ -42,6 +41,42 @@ config.font_rules = {
 }
 
 config.default_prog = { "nu" }
+
+config.ssh_domains = {
+	{
+		name = "GTA-RIL",
+		remote_address = "10.80.55.19",
+		username = "gta",
+		multiplexing = "None",
+
+		-- When multiplexing == "None", default_prog can be used
+		-- to specify the default program to run in new tabs/panes.
+		-- Due to the way that ssh works, you cannot specify default_cwd,
+		-- but you could instead change your default_prog to put you
+		-- in a specific directory.
+		default_prog = { "~/.pixi/bin/nu" },
+
+		-- assume that we can use syntax like:
+		-- "env -C /some/where $SHELL"
+		-- using whatever the default command shell is on this
+		-- remote host, so that shell integration will respect
+		-- the current directory on the remote host.
+		assume_shell = "Posix",
+	},
+}
+
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
+	"nu",
+	"nu.exe",
+	"cmd.exe",
+	"pwsh.exe",
+	"powershell.exe",
+}
 
 -- and finally, return the configuration to wezterm
 return config
