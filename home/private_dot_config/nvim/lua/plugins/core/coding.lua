7a3594b498707eb97038335b0842ef0bf2e9a851
Jack Myers <jack.myers@intel.com>
2025-07-10 23:59:25 +0000

    refactor: better vim lsp diagnostics
    Signed-off-by: Jack Myers <jack.myers@intel.com>

--- home/private_dot_config/nvim/lua/plugins/core/coding.lua
+++ home/private_dot_config/nvim/lua/plugins/core/coding.lua
@@ -26,14 +26,25 @@ return {
     },
     opts = {
       -- Diagnostic configuration
+
       diagnostics = {
-        underline = true,
-        update_in_insert = false,
+        float = { border = 'rounded', source = 'if_many' },
+        underline = { severity = vim.diagnostic.severity.ERROR },
         virtual_text = {
-          spacing = 4,
           source = 'if_many',
+          spacing = 4,
           prefix = '●',
+          format = function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end,
         },
+        update_in_insert = false,
         severity_sort = true,
         signs = true,
       },
@@ -359,32 +370,7 @@ return {
       local function set_vim_diagnostic_config()
         -- Diagnostic Config
         -- See :help vim.diagnostic.Opts
-        vim.diagnostic.config {
-          severity_sort = true,
-          float = { border = 'rounded', source = 'if_many' },
-          underline = { severity = vim.diagnostic.severity.ERROR },
-          signs = vim.g.have_nerd_font and {
-            text = {
-              [vim.diagnostic.severity.ERROR] = '󰅚 ',
-              [vim.diagnostic.severity.WARN] = '󰀪 ',
-              [vim.diagnostic.severity.INFO] = '󰋽 ',
-              [vim.diagnostic.severity.HINT] = '󰌶 ',
-            },
-          } or {},
-          virtual_text = {
-            source = 'if_many',
-            spacing = 2,
-            format = function(diagnostic)
-              local diagnostic_message = {
-                [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                [vim.diagnostic.severity.WARN] = diagnostic.message,
-                [vim.diagnostic.severity.INFO] = diagnostic.message,
-                [vim.diagnostic.severity.HINT] = diagnostic.message,
-              }
-              return diagnostic_message[diagnostic.severity]
-            end,
-          },
-        }
+        vim.diagnostic.config(opts.diagnostics)
       end
       set_vim_diagnostic_config()
 
